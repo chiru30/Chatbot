02ed64dd815fee6af2670158f4cd78580fae243f
Chiranthana R R <73631715+chiru30@users.noreply.github.com>
2021-08-23 23:58:21 +0530

    Update web.py

--- web.py
+++ web.py
@@ -9,3 +9,11 @@ def message_probability(user_message, recognised_words, single_response=False, r
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
