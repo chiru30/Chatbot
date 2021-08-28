f7f7a9f6b42baf64de5b0ac2c2b5b1a0a1624029
Chiranthana R R <73631715+chiru30@users.noreply.github.com>
2021-08-28 23:09:02 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import re
 import long_responses as long
 
+
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words = True
@@ -9,6 +10,7 @@ def message_probability(user_message, recognised_words, single_response=False, r
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
+
     # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
 
@@ -17,11 +19,14 @@ def message_probability(user_message, recognised_words, single_response=False, r
         if word not in user_message:
             has_required_words = False
             break
+
     # Must either have the required words, or be a single response
     if has_required_words or single_response:
         return int(percentage * 100)
     else:
         return 0
+
+
 def check_all_messages(message):
     highest_prob_list = {}
 
@@ -30,4 +35,19 @@ def check_all_messages(message):
         nonlocal highest_prob_list
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
+    # Responses -------------------------------------------------------------------------------------------------------
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
 
+    # Longer responses
+    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # print(highest_prob_list)
+    # print(f'Best match = {best_match} | Score: {highest_prob_list[best_match]}')
+
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
