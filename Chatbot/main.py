6d08d11073fc2828541e49496cc3159f9f180fcd
Chiranthana R R <73631715+chiru30@users.noreply.github.com>
2021-08-30 23:24:49 +0530

    Update main.py

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -26,3 +26,40 @@ def message_probability(user_message, recognised_words, single_response=False, r
     else:
         return 0
 
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    # Simplifies response creation / adds it to the dict
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    # Responses -------------------------------------------------------------------------------------------------------
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+    # Longer responses
+    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # print(highest_prob_list)
+    # print(f'Best match = {best_match} | Score: {highest_prob_list[best_match]}')
+
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+
+# Used to get the response
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+
+# Testing the response system
+while True:
+    print('Bot: ' + get_response(input('You: ')))
